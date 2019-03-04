e186aa10869422df1a2b725595f319ef86faec11
Ben <benjburkholder@gmail.com>
2019-03-04 09:49:54 -0500

    added localization of query

--- serpapi-gmb-crawler-public.py
+++ serpapi-gmb-crawler-public.py
@@ -13,7 +13,16 @@ We can filter out these results down to just the most important fields as well:
 - Address
 - Website
 - GMB Claim status
+- For chain locations, will need to specify city and state in query parameters.
+- Create input area where the user will specify whether or not the locations being scraped are chain/franchise
+    or if they are uniquely named locations. This will govern how the program works.
 """
+
+print('Are the locations chains or uniquely named?')
+print('Chain/Franchise Names (A)')
+print('Unique Names (B)')
+print('-' * 20)
+choice = input('Enter Letter of Choice: ')
 
 downloadFile = 'gmb-status-crawl.csv'
 file = open(downloadFile, 'w')
@@ -21,23 +30,35 @@ file = open(downloadFile, 'w')
 columnHeader = 'Query,Name,Address,Website,GMB Unclaimed?\n'
 file.write(columnHeader)
 
-with open('serp_urls.txt') as content:
+with open('serp_urls.txt') as content, open('serp_locations.txt') as location:
     content = [line.rstrip('\n') for line in content]
+    location = [line.rstrip('\n') for line in location]
 
     for line in content:
-        errorType = ''
-        params = {
-            "q" : str(line),
-            "location" : "United States",
-            "hl" : "en",
-            "gl" : "us",
-            "google_domain" : "google.com",
-            "api_key" : "Purchase API key from SERPApi.com",
-        }
+        for loc in location:
+            errorType = ''
+            if choice == 'B':
+                params = {
+                    "q" : str(line),
+                    "location" : "United States",
+                    "hl" : "en",
+                    "gl" : "us",
+                    "google_domain" : "google.com",
+                    "api_key" : "Enter own API Key from SERP API",
+                }
 
+            elif choice == 'A':
+                params = {
+                    "q": str(line),
+                    "location": f'{loc}, United States',
+                    "hl": "en",
+                    "gl": "us",
+                    "google_domain": "google.com",
+                    "api_key": "32b9a805adc53cc21080d4f3c5a2d7f23f22ea53eeb3875e9b912a73eb794765",
+                }
         query = GoogleSearchResults(params)
         dictionary_results = query.get_dictionary()
-        #print(dictionary_results['knowledge_graph'])
+        # print(dictionary_results['knowledge_graph'])
 
         try:
             name = dictionary_results['knowledge_graph']['title']
